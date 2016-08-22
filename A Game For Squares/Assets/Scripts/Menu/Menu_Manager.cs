23dde1c9898198c9d566aa2cb3fd1254f714ceb7
Ryan Bronk <RPBronk@fullsail.edu>
2016-08-22 11:42:51 -0400

    (the whole game on my side)

---
+++ A Game For Squares/Assets/Scripts/Menu/Menu_Manager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_Manager : MonoBehaviour {
+    public Camera TheCamera; //The Main Camera for the Menu Scene
+
+
+    public enum MENU //Enum that contains all the menus except the Credits Screen (which isn't really a menu)
+    {
+        Title,
+        Main,
+        Options,
+        Controls,
+        Quit
+    };
+    
+    public MENU CurrentMenu;// Self Explanitory
+
+    //**TITLE**//
+    bool isEnterPressed = false;
+    public GameObject T_PressEnter;
+
+    //**MAIN**//
+    //**OPTIONS**//
+    //**CONTROLS**//
+    //**QUIT**//
+    public bool prop_IsEnterPressed
+    {
+        get
+        {
+            return isEnterPressed;
+        }
+
+        set
+        {
+            isEnterPressed = value;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        CurrentMenu = MENU.Title;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    switch(CurrentMenu)
+        {
+            case MENU.Title:
+                {
+                    
+                    TheCamera.transform.position = new Vector3(0, 0, -10);
+                    TheCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
+                    if(Input.GetKeyDown(KeyCode.Return) && isEnterPressed == false)
+                    {
+                        T_PressEnter.GetComponent<Title_PressEnter_Flasher>().DoCoolThing();
+                        
+                    }
+                }
+                break;
+            case MENU.Main:
+                {
+                    TheCamera.transform.position = new Vector3(0, -8, -10);
+                    TheCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
+                }
+                break;
+            case MENU.Options:
+                {
+                    TheCamera.transform.position = new Vector3(8, 0, -10);
+                    TheCamera.transform.localEulerAngles = new Vector3(0, 0, 90);
+                }
+                break;
+            case MENU.Controls:
+                {
+                    TheCamera.transform.position = new Vector3(-8, 0, -10);
+                    TheCamera.transform.localEulerAngles = new Vector3(0, 0, 270);
+                }
+                break;
+            case MENU.Quit:
+                {
+                    TheCamera.transform.position = new Vector3(0, 0, -10);
+                    TheCamera.transform.localEulerAngles = new Vector3(0, 0, 180);
+                }
+                break;
+
+        }
+	}
+ 
+}
---
+++ A Game For Squares/Assets/Scripts/Menu/Title/Title_PressEnter_Shadow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title_PressEnter_Shadow : MonoBehaviour {
+
+    private SpriteRenderer opac; // Opacity Variable
+    private float fade;//Alpha Number
+
+    void Awake()
+    {
+        FindObjectOfType<Menu_Manager>().prop_IsEnterPressed = true;
+        opac = GetComponent<SpriteRenderer>();//This is the sprite and its color pallet
+        fade = 1f;
+    }	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector2(transform.localScale.x + 0.008f, transform.localScale.y + 0.016f);
+
+        fade -= .02f;
+        Color col = opac.color;
+        col.a = fade;
+        opac.color = col;
+        if(fade <= 0f)
+        {
+            FindObjectOfType<Menu_Manager>().prop_IsEnterPressed = false;
+            FindObjectOfType<Menu_Manager>().CurrentMenu = Menu_Manager.MENU.Main;
+            Destroy(gameObject);
+        }
+    }
+}
